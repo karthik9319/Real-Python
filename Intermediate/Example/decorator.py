a55c7ef5587823769a8a72106c9e42eb23b059ac
Karthik <karthik_vullam@cargill.com>
2020-08-15 10:52:27 +0530

    Update decorator.py

--- Intermediate/Example/decorator.py
+++ Intermediate/Example/decorator.py
@@ -8,3 +8,22 @@ def decorator(func):
         # do something after
         return value
     return wrapper_decorator
+
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @functools.wraps(func)
+        def wrapper_repeat(*args, **kwargs):
+            for i in range(num_times):
+                value = func(*args)
+            return value
+        return wrapper_repeat
+    return decorator_repeat
+    
+            
+
+@repeat(num_times = 4)
+def greet(name):
+    print(f"hello {name}")
+
+greet("karthik")
